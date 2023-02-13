41aac98d03caa79c71ecf8071cc9744a9a9e470b
crinazetu <200023627@aston.ac.uk>
2023-02-13 14:51:51 +0000

    added toolbar icons

--- startup.py
+++ startup.py
@@ -3,6 +3,7 @@
 # Also contains some universal functions that will work regardless of what page you're in
 
 import tkinter as tk
+
 from tkinter import messagebox
 import sandbox
 from sandbox import *
@@ -28,16 +29,16 @@ font_size = 12
 global check
 check = tk.StringVar()
 # Sets up toolbar on the top of the window
-global myMenu
-myMenu = tk.Menu()
+
 # Sets up file menu in the toolbar
 global filemenu
-filemenu = tk.Menu(myMenu, tearoff=False)
+filemenu = Frame(root, bd=1, relief=RAISED)
+filemenu.place(x=0, y=0, relwidth=1, height=40)
 
 # Universal frame, this will hold in every page
 global uniFrame
 uniFrame = tk.Frame(root)
-uniFrame.place(x=0, y=0, width=1000, height=720)
+uniFrame.place(x=0, y=40, width=1000, height=680)
 
 # iexit exits the application
 def iexit(event=None):
@@ -47,30 +48,37 @@ def iexit(event=None):
     else:
         pass
 
-# sets buttons up within the top menu with their corresponding functions.
-filemenu.add_command(label='New File',accelerator='Ctrl+N',command=new)
-filemenu.add_command(label='Open File',accelerator='Ctrl+O',command=openfile)
-filemenu.add_command(label='Save',accelerator='Ctrl+S',command=save)
-filemenu.add_command(label='Save as',accelerator='Ctrl+A',command=saveas)
-filemenu.add_command(label='Exit',accelerator='Ctrl+Q',command=iexit)
-myMenu.add_cascade(label='File',menu=filemenu)
-
-
-toolbarmenu = tk.Menu(myMenu, tearoff=False)
-myMenu.add_cascade(label='Toolbars', menu=toolbarmenu)
-
 def course():
     nw = NewWindow.NewWindow('title','label',root)
     nw.tutorial()
 
+# sets buttons up within the top menu with their corresponding functions.
+nficon = PhotoImage(file= r"Sprites\new_file.png")
+nf = Button(filemenu, image=nficon, command=new)
+nf.place(x=0,y=0)
+oficon = PhotoImage(file=r"Sprites\open_file.png")
+of = Button(filemenu, image=oficon, command=openfile)
+of.place(x=36, y=0)
+svicon = PhotoImage(file=r"Sprites\save.png")
+sv = Button(filemenu, image=svicon, command=save)
+sv.place(x=(36*2), y=0)
+svaicon= PhotoImage(file="Sprites\save_as.png")
+sva = Button(filemenu, image=svaicon, command=saveas)
+sva.place(x=(36*3), y=0)
+clicon = PhotoImage(file=r"Sprites\clear.png")
+cl = Button(filemenu, image=clicon, command=clear)
+cl.place(x=(36*4), y=0)
+rnicon = PhotoImage(file=r"Sprites\play.png")
+rn=Button(filemenu, image=rnicon, command=run_code)
+rn.place(x=(36*5), y=0)
+tuticon = PhotoImage(file=r"Sprites\tutorial.png")
+tut = Button(filemenu, image=tuticon, command=course)
+tut.place(x=(36*6), y=0)
 
-toolbarmenu.add_command(label='Course', command=course)
-toolbarmenu.add_command(label='Debug', command='print("debugger")')
 
 
-myMenu.add_command(label='Clear',command=clear)
 
-myMenu.add_command(label='Run',command=run_code)
+
 check.set('light')
 
 # PAGE 1
@@ -88,12 +96,12 @@ def sandboxPage(uniFrame):
 # changepage makes it possible to change the contents of the universal frame
 def changepage():
     global pagenum, root
-    for widget in root.winfo_children():
-        # makes sure the manu is present regardless of what page you're on
-        if root.winfo_children().__contains__(myMenu):
-            continue
-        else:
-            widget.destroy()
+   # for widget in root.winfo_children():
+    # makes sure the manu is present regardless of what page you're on
+        #if root.winfo_children().__contains__(myMenu):
+         #   continue
+       # else:
+         #   widget.destroy()
     if pagenum == 1:
         sandboxPage(root)
         pagenum = 2
@@ -104,15 +112,17 @@ def changepage():
 pagenum = 2
 
 
-root.config(menu=myMenu)
+#root.config(menu=myMenu)
 
 root.bind('<Control-n>',new)
 root.bind('<Control-o>',openfile)
 root.bind('<Control-s>',save)
 root.bind('<Control-a>',saveas)
-root.bind('<Control-q>',iexit)
 root.bind('<Control-p>',font_inc)
 root.bind('<Control-m>',font_dec)
+
+img = PhotoImage(file='Sprites/new_file.png')
+root.wm_iconphoto(True, img)
 
 sandboxPage(uniFrame)
 root.mainloop()
